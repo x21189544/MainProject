1cc47adeea90022537bccfaf5364e5dd98894622
David Scallan-Walsh <x21189544@student.ncirl.ie>
2023-07-26 09:06:39 +0000

    Add Extra EJS Pages

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 const port = 8080;
 
 //Set the view engine to EJS
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
-//Connect to MongoDB Atlas
-mongoose
-  .connect(
-    "mongodb+srv://x21189544:vPl0KPpIukdTT1qW@cluster0.6hyc4wq.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log("Connected to MongoDB Atlas");
-  })
-  .catch((error) => {
-    console.log("Error connecting to MongoDB Atlas: ", error);
-  });
-
-//Define MongoDB schema
-const assetSchema = new mongoose.Schema({
-  assetid: String,
-  make: String,
-  model: String,
-  serial: String,
-});
-
-const Asset = mongoose.model("Asset", assetSchema);
 
 //Serve static files from the 'public' directory
 app.use(express.static("public"));
@@ -48,39 +21,20 @@ app.get("/registerasset", (req, res) => {
   res.render("registerasset");
 });
 
-//send form data to MongoDB
-app.post("/registerasset", (req, res) => {
-  const assetid = req.body.assetid;
-  const make = req.body.make;
-  const model = req.body.model;
-  const serial = req.body.serial;
-
-  //create new asset document
-  const asset = new Asset({
-    assetid: assetid,
-    make: make,
-    model: model,
-    serial: serial,
-  });
-
-  //save asset document to database
-  asset
-    .save()
-    .then(() => {
-      res.send("Asset registered");
-    })
-    .catch((error) => {
-      console.log("Error saving Asset: ".error);
-      res.send("Error registering asset");
-    });
-});
-
 app.get("/updateasset", (req, res) => {
   res.render("updateasset");
 });
 
+app.get("/maintenance", (req, res) => {
+  res.render("maintenance");
+});
+
 app.get("/reports", (req, res) => {
   res.render("reports");
+});
+
+app.get("/transfer", (req, res) => {
+  res.render("transfer");
 });
 
 app.get("/endoflife", (req, res) => {
