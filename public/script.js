b5b1ed5b79207e3c851a7e5caffe0894c51b6459
David Scallan-Walsh <x21189544@student.ncirl.ie>
2023-08-07 18:56:49 +0000

    Add Backgrounds/Tidy Code

---
+++ public/script.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+  el: "#bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x7accfe,
+  backgroundColor: 0x010f18
+});
